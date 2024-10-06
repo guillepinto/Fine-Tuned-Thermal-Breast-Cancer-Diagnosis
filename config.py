c11cdfe0ced24dd898a29efdc1897c8eb0c5f98b
Majinka10 <camachopinto_@hotmail.com>
2024-10-06 16:54:40 -0500

    update device as constant in config

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
 from pathlib import Path
+import torch
 
 # Load environment variables from .env file if it exists
 PROJ_ROOT = Path(__file__).resolve().parent
 
 print(f"PROJ_ROOT path is: {PROJ_ROOT}")
+
+# Device configuration
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 DATA_DIR = PROJ_ROOT / "Imagens e Matrizes da Tese de Thiago Alves Elias da Silva"
 
